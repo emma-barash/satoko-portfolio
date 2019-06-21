a21f10b8147130e10c0419867a94b10a36aba2d8
Emma Barash <emma.barash.dev@gmail.com>
2019-06-20 20:41:31 -0600

    ready for Heroku

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ require('dotenv').config()
 app.use(express.json());
 app.use(morgan('dev'));
 
+// ... other imports
+const path = require("path")
+
+// ... other app.use middleware setups
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 // CONNECT TO THE DB    
-mongoose.connect('mongodb://localhost:27017/token-auth-1', 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/token-auth-1', 
 { useNewUrlParser: true },
 () => console.log('Connected to the DB'));
 
@@ -30,6 +36,13 @@ app.use((err, req, res, next) => {
     console.error(err)
     return res.status(500).send({ errMsg: err.message })
 })
+
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // SERVER LISTENING
 app.listen(PORT, () => {
